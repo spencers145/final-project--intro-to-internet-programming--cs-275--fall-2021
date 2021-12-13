139ad89d4eec82f5bf6e4546fdb2901bd06bf5a8
spencers145 <smartypantsspencer@gmail.com>
2021-12-12 19:21:28 -0500

    Add Gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,198 @@
+const { src, dest, series, watch } = require(`gulp`),
+    CSSLinter = require(`gulp-stylelint`),
+    del = require(`del`),
+    babel = require(`gulp-babel`),
+    htmlCompressor = require(`gulp-htmlmin`),
+    htmlValidator = require(`gulp-html`),
+    jsCompressor = require(`gulp-uglify`),
+    jsLinter = require(`gulp-eslint`),
+    browserSync = require(`browser-sync`),
+    reload = browserSync.reload;
+
+let browserChoice = `default`;
+
+async function brave () {
+    browserChoice = `brave browser`;
+}
+
+async function chrome () {
+    browserChoice = `google chrome`;
+}
+
+async function edge () {
+    // In Windows, the value might need to be “microsoft-edge”. Note the dash.
+    browserChoice = `microsoft edge`;
+}
+
+async function firefox () {
+    browserChoice = `firefox`;
+}
+
+async function opera () {
+    browserChoice = `opera`;
+}
+
+async function safari () {
+    browserChoice = `safari`;
+}
+
+async function vivaldi () {
+    browserChoice = `vivaldi`;
+}
+
+async function allBrowsers () {
+    browserChoice = [
+        `brave browser`,
+        `google chrome`,
+        `microsoft edge`, // Note: In Windows, this might need to be microsoft-edge
+        `firefox`,
+        `opera`,
+        `safari`,
+        `vivaldi`
+    ];
+}
+
+let validateHTML = () => {
+    return src([`*.html`])
+        .pipe(htmlValidator(undefined));
+};
+
+let lintJS = () => {
+    return src(`js/*.js`)
+        .pipe(jsLinter())
+        .pipe(jsLinter.formatEach(`compact`));
+};
+
+let transpileJSForDev = () => {
+    return src(`js/*.js`)
+        .pipe(babel())
+        .pipe(dest(`temp/js`));
+};
+
+let compressHTML = () => {
+    return src([`*.html`])
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
+
+let transpileJSForProd = () => {
+    return src(`js/*.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/js`));
+};
+
+let serve = () => {
+    browserSync({
+        notify: true,
+        reloadDelay: 50,
+        browser: browserChoice,
+        server: {
+            baseDir: [
+                `temp`,
+                `./`
+            ]
+        }
+    });
+
+    watch(`js/*.js`, series(lintJS, transpileJSForDev))
+        .on(`change`, reload);
+
+    watch(`*.html`, validateHTML)
+        .on(`change`, reload);
+
+    watch(`css/*.css`, lintCSS)
+        .on(`change`, reload);
+
+    watch(`img/*`)
+        .on(`change`, reload);
+};
+
+async function clean() {
+    let fs = require(`fs`),
+        i,
+        foldersToDelete = [`./temp`, `prod`];
+
+    for (i = 0; i < foldersToDelete.length; i++) {
+        try {
+            fs.accessSync(foldersToDelete[i], fs.F_OK);
+            process.stdout.write(`\n\tThe ` + foldersToDelete[i] +
+                ` directory was found and will be deleted.\n`);
+            del(foldersToDelete[i]);
+        } catch (e) {
+            process.stdout.write(`\n\tThe ` + foldersToDelete[i] +
+                ` directory does NOT exist or is NOT accessible.\n`);
+        }
+    }
+
+    process.stdout.write(`\n`);
+}
+
+async function listTasks () {
+    let exec = require(`child_process`).exec;
+
+    exec(`gulp --tasks`, function (error, stdout, stderr) {
+        if (null !== error) {
+            process.stdout.write(`An error was likely generated when invoking ` +
+                `the “exec” program in the default task.`);
+        }
+
+        if (`` !== stderr) {
+            process.stdout.write(`Content has been written to the stderr stream ` +
+                `when invoking the “exec” program in the default task.`);
+        }
+
+        process.stdout.write(`\n\tThis default task does ` +
+            `nothing but generate this message. The ` +
+            `available tasks are:\n\n${stdout}`);
+    });
+}
+
+let lintCSS = () => {
+    return src(`css/*.css`)
+        .pipe(CSSLinter({
+            failAfterError: false,
+            reporters: [
+                {formatter: `string`, console: true}
+            ]
+        }));
+};
+
+let copyCSS = () => {
+    return src(`css/*.css`)
+        .pipe(dest(`prod/css`));
+};
+
+let copyImages = () => {
+    return src([`img/*.png`, `img/*.svg`])
+        .pipe(dest(`prod/img`));
+};
+
+exports.brave = series(brave, serve);
+exports.chrome = series(chrome, serve);
+exports.edge = series(edge, serve);
+exports.firefox = series(firefox, serve);
+exports.opera = series(opera, serve);
+exports.safari = series(safari, serve);
+exports.vivaldi = series(vivaldi, serve);
+exports.allBrowsers = series(allBrowsers, serve);
+exports.validateHTML = validateHTML;
+exports.lintJS = lintJS;
+exports.transpileJSForDev = transpileJSForDev;
+exports.compressHTML = compressHTML;
+exports.transpileJSForProd = transpileJSForProd;
+exports.clean = clean;
+exports.default = listTasks;
+exports.lintCSS = lintCSS;
+exports.serve = series(
+    validateHTML,
+    lintJS,
+    transpileJSForDev,
+    serve
+);
+exports.build = series(
+    compressHTML,
+    transpileJSForProd,
+    copyCSS,
+    copyImages,
+);
